e7e17fba599ef5f23556e5ed883d9fe3844c8e92
Will Ward <will.j.ward3@gmail.com>
2019-06-09 22:58:12 +0900

    Added User, Home and Layout components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React from "react"
 
-import Header from "./Header"
-import ThoseGuys from "./ThoseGuys"
+import Home from "./Home"
 import "./app.css"
 
 const App = () => {
     return (
         <div>
-            <Header />
-            <h1 id="info">Bears Team 30 App!</h1>
-            <ThoseGuys />
+            <Home />
         </div>
     )
 }
