ebe086e03d06f206d99ddc4fd5fe4ffb025aabc9
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-10 15:53:05 +0800

    wraps App in Provider

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import App from "./components/App";
 import { Provider } from 'react-redux';
 import { store } from './components/store';
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("app"));
 
