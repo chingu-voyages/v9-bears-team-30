3b22476fabcd27e300fa4cf02a1f2633e25036e9
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-10 15:50:50 +0800

    imports provider and store to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./components/App"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from 'react-redux';
+import { store } from './components/store';
 
 ReactDOM.render(<App />, document.getElementById("app"))
 
