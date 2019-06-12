65ca78429f0351d48240122d1ec985e0393c5b35
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-12 16:45:59 +0800

    adds putSignupReducer to rootReducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { titleReducer } from './titleReducer';
 import { signupEmailReducer } from './signupEmailReducer';
 import { signupPasswordReducer } from './signupPasswordReducer';
+import { putSignupReducer } from './putSignupReducer';
 
 export const rootReducer = combineReducers({
 	title: titleReducer,
 	signupEmail: signupEmailReducer,
-	signupPassword: signupPasswordReducer
+	signupPassword: signupPasswordReducer,
+	putSignup: putSignupReducer
 });
