eb9b83f9d5d00df507c1d595debe91530566a4f8
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-12 14:51:43 +0800

    adds new reducers to rootReducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import { titleReducer } from './titleReducer';
+import { signupEmailReducer } from './signupEmailReducer';
+import { signupPasswordReducer } from './signupPasswordReducer';
 
 export const rootReducer = combineReducers({
-	title: titleReducer
+	title: titleReducer,
+	signupEmail: signupEmailReducer,
+	signupPassword: signupPasswordReducer
 });
