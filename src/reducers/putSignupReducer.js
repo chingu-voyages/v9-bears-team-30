61a3805fe7065ce38741db66970f0932f34adad5
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-12 16:44:37 +0800

    adds putSignupReducer

---
+++ src/reducers/putSignupReducer.js
@@ -0,0 +1,29 @@
+import { 
+  PUT_SIGNUP_SUCCESS, 
+  PUT_SIGNUP_FAILURE, 
+  PUT_SIGNUP_STARTED } from '../actions/putSignupAction';
+
+export const putSignupReducer = (state = {saving: false, newSignup: [], error: null}, action) => {
+  switch (action.type) {
+    case PUT_SIGNUP_STARTED:
+      return {
+        ...state,
+        saving: true
+      };
+    case PUT_SIGNUP_SUCCESS:
+      return {
+        ...state,
+        saving: false,
+        error: null,
+        newSignup: [...state.newSignup, action.payload]
+      };
+    case PUT_SIGNUP_FAILURE:
+      return {
+        ...state,
+        saving: false,
+        error: action.payload.error
+      };
+    default:
+      return state;
+  }
+}
