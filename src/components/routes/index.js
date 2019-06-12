b49b6e072d51e92939d93ada8ddb4bb22e36fc2d
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-12 17:14:15 +0800

    adds SignUpPage component as Route /signup to index.js

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Home from "./home";
-import User from "./Users"
+import User from "./Users";
+import SignUpPage from "./SignUpPage"
 
 export default () => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={Home} />
       <Route path="/users" component={User} />
+      <Route path="/signup" component={SignUpPage} />
     </BrowserRouter>
   );
 };
