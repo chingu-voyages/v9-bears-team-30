4e8b6acd3387821e99f65c8e1c34820cb476575a
Will Ward <will.j.ward3@gmail.com>
2019-06-14 01:00:07 +0900

    Added url-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
-      }
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {},
+          },
+        ],
+      },
     ]
   },
   devServer: {
