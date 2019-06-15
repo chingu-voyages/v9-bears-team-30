46c63b826fc786bfd5522a41b573eaf3b6cc1e74
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-15 13:32:55 +0800

    adds SignupSuccess component

---
+++ src/components/SignupSuccess.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './signup.css';
+
+export class SignUpSuccess extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div className='signup-page'>
+        <div className='signup-container'>
+          <div className='signup-heading-container'>
+            <h1 className='signup-heading-h1'>Success!</h1>
+          </div>
+          <div className='signup-form'>
+            <div className='user-input-container'>
+              <p>Thank you for joining ClimateSpy!</p>
+              <a href="">Go back</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
