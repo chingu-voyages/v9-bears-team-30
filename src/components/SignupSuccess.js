34ce0c08b3538deec51057cbc4009038ee1a42ba
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-15 13:48:13 +0800

    removes outer div from SignupSuccess.js

--- src/components/SignupSuccess.js
+++ src/components/SignupSuccess.js
@@ -10,16 +10,14 @@ export class SignUpSuccess extends Component {
   render() {
 
     return (
-      <div className='signup-page'>
-        <div className='signup-container'>
-          <div className='signup-heading-container'>
-            <h1 className='signup-heading-h1'>Success!</h1>
-          </div>
-          <div className='signup-form'>
-            <div className='user-input-container'>
-              <p>Thank you for joining ClimateSpy!</p>
-              <a href="">Go back</a>
-            </div>
+      <div className='signup-container'>
+        <div className='signup-heading-container'>
+          <h1 className='signup-heading-h1'>Success!</h1>
+        </div>
+        <div className='signup-form'>
+          <div className='user-input-container'>
+            <p>Thank you for joining ClimateSpy!</p>
+            <a href="">Go back</a>
           </div>
         </div>
       </div>
