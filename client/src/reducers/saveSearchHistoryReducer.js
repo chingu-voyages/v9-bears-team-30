0e08b75b9a636fbdd60fdcbfb6824a4fc304d0bd
Ethan Vernon <ethan.vernon@colorado.edu>
2019-07-06 19:31:23 -0700

    adds cases to saveSearchHistoryReducer

--- client/src/reducers/saveSearchHistoryReducer.js
+++ client/src/reducers/saveSearchHistoryReducer.js
@@ -4,13 +4,24 @@ import {
   SAVE_SEARCH_HISTORY_FAILURE } from '../actions/saveSearchHistoryAction';
 
 //searchHistory is an array, when reducer receives save action, pushes search to end of array
-export const saveSearchHistoryReducer = (state = {searchHistory: [], action) => {
+export const saveSearchHistoryReducer = (state = { searchHistory: [], error: null }, action) => {
   switch (action.type) {
     case SAVE_SEARCH_HISTORY :
-    return { 
+      return { 
+          ...state,
+          searchHistory: [...state.searchHistory, action.searchCityAndState]
+      }
+    case SAVE_SEARCH_HISTORY_SUCCESS :
+      return {
         ...state,
-        searchHistory: [...state.searchHistory, action.searchCityAndState]
-    }
+        error: null,
+        searchHistory: [...state.searchHistory, action.payload]
+      };
+    case SAVE_SEARCH_HISTORY_FAILURE :
+      return {
+        ...state,
+        error: action.payload.error.response.data
+      };
     default:
       return state;
   }
