db41479bb41a007513e5ee0215cd1e314d9742ba
Ethan Vernon <ethan.vernon@colorado.edu>
2019-07-06 19:28:59 -0700

    add improted actions to saveSearchHistoryReducer

--- client/src/reducers/saveSearchHistoryReducer.js
+++ client/src/reducers/saveSearchHistoryReducer.js
@@ -1,10 +1,12 @@
 import { 
-  SAVE_SEARCH_HISTORY } from '../actions/saveSearchHistoryAction';
+  SAVE_SEARCH_HISTORY, 
+  SAVE_SEARCH_HISTORY_SUCCESS,
+  SAVE_SEARCH_HISTORY_FAILURE } from '../actions/saveSearchHistoryAction';
 
 //searchHistory is an array, when reducer receives save action, pushes search to end of array
 export const saveSearchHistoryReducer = (state = {searchHistory: [], action) => {
   switch (action.type) {
-    case SAVETOUSERSEARCHHISTORY :
+    case SAVE_SEARCH_HISTORY :
     return { 
         ...state,
         searchHistory: [...state.searchHistory, action.searchCityAndState]
